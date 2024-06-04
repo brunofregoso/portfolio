148a4d0ce684180b7643f36ffc556c6caa91dfc3
Bruno Fregoso Frias <bruno88756@gmail.com>
2024-06-04 00:47:42 -0400

    background of main and flexbox for taskbar

---
+++ components/header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="fixed top-[20px] left-0 right-0 flex justify-center">
+      <div className="flex gap-20 flex-row justify-center rounded-full w-[600px] h-[40px] border-black bg-gray-50">
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+      </div>
+    </div>
+  );
+}
